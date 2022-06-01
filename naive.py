27385102112bdd7443ac2c7bf52f8bfce9ec7f0c
Trenton-Ruf <truf@live.com>
2022-06-01 13:33:33 -0700

    added simple results output

--- naive.py
+++ naive.py
@@ -60,6 +60,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Train Data
 print("Training")
 y_pred = gnb.fit(X_train, y_train).predict(X_test)
+
+print("Number of mislabeled points out of a total %d points : %d"% (X_test.shape[0], (y_test != y_pred).sum()))
 ############################
 
 #Confusion Matrix

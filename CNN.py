b5d70f00b8d979da7f66d44192254ef62694fa22
Alec-D <105760187+Alec-D@users.noreply.github.com>
2022-06-07 09:19:54 -0700

    Add files via upload

--- CNN.py
+++ CNN.py
@@ -1,17 +1,17 @@
 # Alec Diefenbaugh & Austin Britton
-# CS 445
+# CS 445/545
 # Card Recognition Project CNN
 
 import tensorflow as tf
+import numpy as np  # for vector & matrix math operations, also has its own random number generator
+import pandas as pd  # pandas loading of csv is much quicker than np.loadtext! (5 sec vs 38 sec)
+import matplotlib.pyplot as plt  # for displaying graphs of accuracy per epoch
+import seaborn # for displaying confusion matrices
 
 # import pathlib
 # import os
 # from tensorflow import keras
 # from tensorflow.keras import layers
-# import numpy as np  # for vector & matrix math operations, also has its own random number generator
-# import pandas as pd  # pandas loading of csv is much quicker than np.loadtext! (5 sec vs 38 sec)
-# import matplotlib.pyplot as plt  # for displaying graphs of accuracy per epoch
-# import seaborn # for displaying confusion matrices
 # import PIL
 # import PIL.Image
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -34,7 +34,7 @@ valDS = tf.keras.utils.image_dataset_from_directory(
     subset="validation",
     seed=123,
     image_size=(40, 40),
-    batch_size=3
+    # batch_size=3
 )
 
 model = tf.keras.models.Sequential()
@@ -57,26 +57,36 @@ loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 optim = tf.keras.optimizers.Adam(learning_rate=0.001)
 metrics = ["accuracy"]
 model.compile(optimizer=optim, loss=loss, metrics=metrics)
-model.fit(trainDS, epochs=10, verbose=2)
+history = model.fit(trainDS, epochs=50, verbose=2, validation_data=valDS)
 model.evaluate(valDS, verbose=2)
+accuracy = history.history['val_accuracy']
 
-y_pred=model.predict_classes(valDS)
-con_mat = tf.math.confusion_matrix(labels=y_true, predictions=y_pred).numpy()
 
-con_mat_norm = np.around(con_mat.astype('float') / con_mat.sum(axis=1)[:, np.newaxis], decimals=2)
+# Stuff to generate confusion matrix & accuracy per epoch plot:
 
-con_mat_df = pd.DataFrame(con_mat_norm,
-                     index = classes, 
-                     columns = classes)
+preds = []
+true = []
+for images, labels in valDS:
+    preds.append(tf.argmax(model.predict(images), axis=1))
+    true.append(labels)
+preds = np.concatenate(preds)
+true = np.concatenate(true)
 
-print(con_mat_df)
-#I cannot download matplotlib on my linux machine So i am hoping someone else can test the actual heat map
-'''
-figure = plt.figure(figsize=(8, 8))
-sns.heatmap(con_mat_df, annot=True,cmap=plt.cm.Blues)
+classes = trainDS.class_names
+con_mat = tf.math.confusion_matrix(labels=true, predictions=preds).numpy()
+
+fig1 = plt.figure()
+plt.title("Test Data Confusion Matrix")
+seaborn.set(font_scale=0.7)
+seaborn.heatmap(con_mat, cmap="Blues", linewidth=0.5, annot=True, xticklabels=classes, yticklabels=classes)
 plt.tight_layout()
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
-plt.show()
+plt.xlabel('Predicted Class')
+plt.ylabel('True Class')
 
-'''
+fig2 = plt.figure()
+plt.title('Accuracy per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy (%)')
+plt.plot(accuracy,'b',marker='.')
+
+plt.show(block=True)
